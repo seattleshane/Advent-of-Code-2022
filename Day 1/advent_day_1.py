417a7ecfd33f19e35a2155f1cc681f46e2b521e9
seattleshane <shane.a.rideout@gmail.com>
2022-12-03 20:21:19 -0800

    Create advent_day_1.py
    advent day 1

---
+++ Day 1/advent_day_1.py
@@ -0,0 +1,49 @@
+def get_values_from_file(path_to_file: str) -> list[str]:
+    """Takes a path to a text file and returns the lines as a list of strings.
+
+    Args:
+        path_to_file (str): Absolute path to file.
+
+    Returns:
+        list[str]: Lines as a list of strings
+    """
+    ruck_sacks = []
+    with open(path_to_file) as file:
+        lines = file.readlines()
+        stripped_lines = [line.rstrip() for line in lines]
+        return stripped_lines
+
+def part_one(lines: list[str]) -> int:
+    highest_calories: int = 0
+    calories: int = 0
+    for i in range(len(lines)):
+        if lines[i]:
+            calories += int(lines[i])
+        else:
+            if calories >= highest_calories:
+                highest_calories = calories
+                calories = 0
+            else:
+                calories = 0
+    return highest_calories
+
+def part_two(lines: list[str]) -> int:
+    calories: int = 0
+    top_3_highest_calories: list[int] = [0,0,0]
+    for i in range(len(lines)):
+        if lines[i]:
+            calories += int(lines[i])
+        else:
+            top_3_highest_calories.sort()
+            for index, highest_calorie in enumerate(top_3_highest_calories):
+                if calories >= highest_calorie:
+                    top_3_highest_calories[index] = calories
+                    calories = 0
+                    break
+                else:
+                    calories = 0
+    return sum(top_3_highest_calories)
+
+if __name__ == "__main__":
+    lines = get_values_from_file("path")
+    print(part_two(lines))
