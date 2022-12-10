3fd7a64d6ebe69bd62b09deb290f0f16d917c050
seattleshane <shane.a.rideout@gmail.com>
2022-12-10 11:13:37 -0800

    Part 2

--- Day10/advent_day_10.py
+++ Day10/advent_day_10.py
@@ -31,7 +31,41 @@ def part_one(lines: list[str]) -> int:
     return sum(signals_of_interest)
 
 def part_two(lines: list[str]):
-    pass
+    x: int = 1
+    cycle: int = 0
+    signals_of_interest: list[int] = []
+    debug_list: list[tuple[int, str]] = []
+    for instruction in lines:
+        if len(instruction.split(" ")) == 1:
+            cycle += 1
+            debug_list.append((cycle,  x, instruction, (cycle * x), 0))
+            calculate_signal_strength(cycle, x, signals_of_interest,)
+        else:
+            cycle += 1
+            debug_list.append((cycle,  x, instruction, (cycle * x), int(instruction.split(" ")[1])))
+            calculate_signal_strength(cycle, x, signals_of_interest,)
+            cycle += 1
+            debug_list.append((cycle,  x, instruction, (cycle * x), int(instruction.split(" ")[1])))
+            calculate_signal_strength(cycle, x, signals_of_interest,)
+            x += int(instruction.split(" ")[1])
+    line: str = ""
+    counter = 0
+    for item in debug_list:
+        if (
+            counter == item[1] + 1 or
+            counter == item[1] - 1 or
+            counter == item[1]
+        ):
+            line += "#"
+        else:
+            line += "."
+        counter += 1
+        if counter % 40 == 0:
+            print(str(line))
+            line = ""
+            counter = 0
+
+            
 
 def calculate_signal_strength(cycle: int, x: int, signals: list[int]) -> int:
     if cycle == 20:
